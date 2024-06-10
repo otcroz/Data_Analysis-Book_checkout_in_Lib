e901bd77dba9e11557dc3336a20e73128148a1aa
otcroz <dbtndus0527@gmail.com>
2024-06-11 04:07:15 +0900

    [feat] Data manipulation: A book borrowing list in nationwide library - filter seoul library and group by member - add to column a borrowing book count

--- data_manipulation.py
+++ data_manipulation.py
@@ -19,7 +19,29 @@ lib_info_seoul_data.info()
 lib_info_seoul_data.to_csv('./clean_data/서울시_공공_도서관_정보.csv', sep=',', index=False, encoding="utf-8-sig")
 
 
+# In[] 도서관 대출내역 불러오기
 
+
+lib_borrow = pd.read_csv('./raw_data/전국_공공_대출내역_202312-4.csv', encoding="utf-8")
+lib_info_seoul_data = pd.read_csv('./clean_data/서울시_공공_도서관_정보.csv', encoding="utf-8")
+
+lib_borrow.info()
+
+# if 서울시 도서관에서 대출한 내역에 해당하는 데이터
+lib_borrow_seoul = lib_borrow[lib_borrow.LBRRY_CD.isin(lib_info_seoul_data.LBRRY_CD)]
+
+lib_borrow_seoul.info()
+
+# 회원번호 grouping, count한 값을 추가 
+borrow_counts = lib_borrow_seoul.groupby(['MBER_SEQ_NO_VALUE', 'LON_DE']).size().reset_index(name='BORROW_COUNT')
+
+lib_borrow_seoul = lib_borrow_seoul[['LBRRY_CD', 'MBER_SEQ_NO_VALUE', 'LON_DE']]
+
+lib_borrow_seoul = lib_borrow_seoul.merge(borrow_counts, on=['MBER_SEQ_NO_VALUE', 'LON_DE'], how='left')
+
+# check
+print(lib_borrow_seoul.groupby(['MBER_SEQ_NO_VALUE', 'LON_DE'])['BORROW_COUNT'].count())
+print(pd.DataFrame((lib_borrow_seoul.groupby(['MBER_SEQ_NO_VALUE', 'LON_DE'])['BORROW_COUNT'])).shape)
 
 
 
