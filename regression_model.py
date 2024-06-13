fb2ac64024508ced8c849dabfd30835449f78bd9
otcroz <dbtndus0527@gmail.com>
2024-06-13 23:54:53 +0900

    [comment] Add to code guideline

---
+++ regression_model.py
@@ -0,0 +1,32 @@
+# In[] 데이터 로드
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('')
+
+# In[] 데이터 상관관계 히트맵
+
+
+# In[] 데이터 분할
+
+
+# In[] 데이터 인코딩
+
+
+
+# In[] 데이터 스케일링
+
+
+
+# In[] 1. 모델: linear regression model
+
+
+
+# In[] 2. 모델: decision tree regression
+
+
+
+# In[] 3. 모델: random forest regression
+
+
+
